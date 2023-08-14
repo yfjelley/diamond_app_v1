b51eba6d96d77f37bd7c1ca27a95763599c951ce
yfjelley <9162896+yfjelley@user.noreply.gitee.com>
2023-08-14 19:53:22 +0800

    fix bug

--- pages/tactics/tactics.vue
+++ pages/tactics/tactics.vue
@@ -446,7 +446,12 @@
 											</view>
 											<img class="avatar" :src="host + item.user.avatar" mode=""></img>
 											<view class="name">
-												{{item.name}}
+												{{item.user.nickname}}
+											</view>
+											<view class="title">
+												<view class="name">
+													{{item.name}}
+												</view>
 											</view>
 										</view>
 										<text class="grid">合约网格</text>
